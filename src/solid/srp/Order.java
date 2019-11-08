5711f16895cf1428f4ea83ac38f93dbd887bba94
LiubomyrPater <pater87lm@gmail.com>
2019-11-08 18:30:41 +0200

    Test add

---
+++ src/solid/srp/Order.java
@@ -0,0 +1,7 @@
+package solid.srp;
+
+public class Order {
+    public String name;
+    public String email;
+
+}
---
+++ src/solid/srp/Process.java
@@ -0,0 +1,8 @@
+package solid.srp;
+
+public class Process {
+
+    public void order (Order order){
+        System.out.println("Receive order");
+    }
+}
