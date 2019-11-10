4ee1721fd34e5e822170d8c6df840a640a9874af
LiubomyrPater <pater87lm@gmail.com>
2019-11-10 16:12:47 +0200

    Test add

--- src/solid/srp/Process.java
+++ src/solid/srp/Process.java
@@ -5,4 +5,7 @@ public class Process {
     public void order (Order order){
         System.out.println("Receive order");
     }
+    public void orderT (Order order){
+        System.out.println("Receive order");
+    }
 }
