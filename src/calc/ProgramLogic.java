bb4eab1e2215f6ca4035d71d50a8e3230fd78ba6
LiubomyrPater <pater87lm@gmail.com>
2019-11-15 17:27:48 +0200

    Maybe the end

--- src/calc/ProgramLogic.java
+++ src/calc/ProgramLogic.java
@@ -37,34 +37,30 @@ public abstract class ProgramLogic {
     }
 
     private static void calcProgram(int theDigit){
-        boolean programCalc = true;
-        while (programCalc){
-            PrintFrases.newCalcAction();
-            Scanner scanner = new Scanner(System.in);
-            if (scanner.hasNext("[<]")){
-                programCalc = false;
+        PrintFrases.newCalcAction();
+        Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNext("[<]")){
+            ParsedInputData row = new ParsedInputData(scanner.nextLine());
+            if (row.getAct().contains("+")){
+                new Addiction(row.getX(), row.getY(), theDigit);
+            }else if (row.getAct().contains("-")){
+                new Subtraction(row.getX(), row.getY(), theDigit);
+            }else if (row.getAct().contains("*")){
+                new Multiplicate(row.getX(), row.getY(), theDigit);
+            }else if (row.getAct().contains("/")){
+                new Divide(row.getX(), row.getY(), theDigit);
+            }else if (row.getAct().contains("!")){
+                new Plug();
+            }else if (row.getAct().contains("%")){
+                new DivideByModul(row.getX(), row.getY(), theDigit);
+            }else if (row.getAct().contains("S")) {
+                new Sqrt(row.getX(), theDigit);
+            }else if (row.getAct().contains("^")){
+                new Pow(row.getX(), row.getY(), theDigit);
             }else {
-                ParsedInputData row = new ParsedInputData(scanner.nextLine());
-                if (row.getAct().contains("+")){
-                    new Addiction(row.getX(), row.getY(), theDigit);
-                }else if (row.getAct().contains("-")){
-                    new Subtraction(row.getX(), row.getY(), theDigit);
-                }else if (row.getAct().contains("*")){
-                    new Multiplicate(row.getX(), row.getY(), theDigit);
-                }else if (row.getAct().contains("/")){
-                    new Divide(row.getX(), row.getY(), theDigit);
-                }else if (row.getAct().contains("!")){
-                    new Plug();
-                }else if (row.getAct().contains("%")){
-                    new DivideByModul(row.getX(), row.getY(), theDigit);
-                }else if (row.getAct().contains("S")) {
-                    new Sqrt(row.getX(), theDigit);
-                }else if (row.getAct().contains("^")){
-                    new Pow(row.getX(), row.getY(), theDigit);
-                }else {
-                    new Plug();
-                }
+                new Plug();
             }
+            ProgramLogic.calcProgram(theDigit);
         }
     }
 }
