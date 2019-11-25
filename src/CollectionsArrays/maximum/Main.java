d29791efac218fc51103b4038f0e3eed22c05ac0
LiubomyrPater <pater87lm@gmail.com>
2019-11-25 13:31:12 +0200

    ArrayListCars

--- src/CollectionsArrays/maximum/Main.java
+++ src/CollectionsArrays/maximum/Main.java
@@ -6,33 +6,27 @@ import java.util.*;
 
 public class Main {
 
-//    План максимум:
-//-----------------------------------------------------------------------------------------------
-//    Створити сутністності Авто, Кермо, Двигун.
-
-//    Створити двовимірний масив, величина якого вираховується рандомно.
-
-//    Заповнити даний масив обєктами авто, всі значення конструктора якого заповнюються рандомно.
-
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        List<List> cars1 = new ArrayList<>();
+        List<List> cars = new ArrayList<>();
         for (int i = 0; i <(random.nextInt(5) + 5) ; i++) {
-            cars1.add(new ArrayList());
+            cars.add(new ArrayList());
         }
-        for (int j = 0; j <cars1.size() ; j++) {
+        for (int j = 0; j <cars.size() ; j++) {
             for (int i = 0; i <(random.nextInt(5) + 5) ; i++) {
-                cars1.get(cars1.size()-1).add(new Car(random));
+                cars.get(j).add(new Car(random));
             }
         }
+        Realised.mainMenuList(scanner, cars, random);
 
 
 
 
+
+//реалізація через двовтмірний масив
+/*
         Car[][] cars = new Car[random.nextInt(10) + 10][];
         for (int i = 0; i < cars.length ; i++) {
             cars[i] = new Car[random.nextInt(5) + 10];
@@ -40,6 +34,6 @@ public class Main {
                 cars[i][j] = new Car(random);
             }
         }
-        Realised.mainMenu(scanner, cars, random);
+        Realised.mainMenu(scanner, cars, random);*/
     }
 }
--- src/CollectionsArrays/maximum/Realised.java
+++ src/CollectionsArrays/maximum/Realised.java
@@ -14,6 +14,107 @@ import java.util.*;
 
 public abstract class Realised {
 
+    private static Comparator choiceField(Scanner scanner){
+
+        while (true){
+            scanner.reset();
+            System.out.print("Input name of field for sorting (engine  helm  year): ");
+            String field = scanner.next();
+            if (field.matches("engine")){
+                return new CarEngineComparator();
+            }else if (field.matches("helm")){
+                return new CarHelmComparator();
+            }else if (field.matches("year")){
+                return new CarYearComparator();
+            }else{
+                System.out.println("Try again");
+            }
+        }
+    }
+
+
+    private static List sortByAnyList(List cars, boolean upDown, Comparator comparator){
+        if (upDown){
+            Iterator<List> cars1 = cars.iterator();
+            while (cars1.hasNext()){
+                cars1.next().sort(comparator);
+            }
+        }else {
+            Iterator<List> cars1 = cars.iterator();
+            while (cars1.hasNext()){
+                cars1.next().sort(Collections.reverseOrder(comparator));
+            }
+        }
+        return cars;
+    }
+
+    public static void mainMenuList(Scanner scanner, List cars, Random random){
+        while (true){
+            System.out.print("\n" + "Choice, please - " + "\n" +
+                    "1) Print all array" + "\n" +
+                    "2) Same field" + "\n" +
+                    "3) Sorting in descending order by any field" + "\n" +
+                    "4) Sorting in ascending order by any field" + "\n" +
+                    "<  for exit" + "\n" +
+                    ": ");
+            String chapter = scanner.next();
+            if (chapter.matches("[1234<]")){
+                if (chapter.matches("[<]")){
+                    System.exit(0);
+                }else {
+                    choiceMenuList(Integer.valueOf(chapter), cars, scanner, random);
+                }
+            }else {
+                System.out.println("Only: 1 2 3 4");
+            }
+        }
+    }
+
+    private static void theSameValueList(List cars, Random random){
+        Iterator<List> cars1 = cars.iterator();
+        while (cars1.hasNext()){
+            List<Car> cars2 = cars1.next();
+            cars2.clear();
+            for (int i = 0; i < (random.nextInt(5) + 5) ; i++) {
+                cars2.add(new Car(random, 1900));
+            }
+        }
+    }
+
+    public static void viewList(List cars1){
+        Iterator<List> carsList = cars1.listIterator();
+        while (carsList.hasNext()){
+            List<Car> cars = carsList.next();
+            Iterator<Car> cars2 = cars.iterator();
+            while (cars2.hasNext()){
+                System.out.println(cars2.next().toString());
+            }
+        }
+    }
+
+    private static void choiceMenuList(int chapter, List cars1, Scanner scanner, Random random){
+        switch (chapter){
+            case 1:{
+                viewList(cars1);
+                break;
+            }case 2:{
+                theSameValueList(cars1, random);
+                break;
+            }case 3:{
+                List cars = sortByAnyList(cars1, true, choiceField(scanner));
+                viewList(cars);
+                break;
+            }case 4:{
+                List cars = sortByAnyList(cars1, false, choiceField(scanner));
+                viewList(cars);
+                break;
+            }
+        }
+    }
+
+
+
+
     private static Car[][] sortByAny(Car [][] cars, boolean upDown, Comparator comparator){
         if (upDown){
             for (int i = 0; i <cars.length ; i++) {
@@ -45,24 +146,6 @@ public abstract class Realised {
                 }
             }else {
                 System.out.println("Only: 1 2 3 4");
-            }
-        }
-    }
-
-    private static Comparator choiceField(Scanner scanner){
-
-        while (true){
-            scanner.reset();
-            System.out.print("Input name of field for sorting (engine  helm  year): ");
-            String field = scanner.next();
-            if (field.matches("engine")){
-                return new CarEngineComparator();
-            }else if (field.matches("helm")){
-                return new CarHelmComparator();
-            }else if (field.matches("year")){
-                return new CarYearComparator();
-            }else{
-                System.out.println("Try again");
             }
         }
     }
