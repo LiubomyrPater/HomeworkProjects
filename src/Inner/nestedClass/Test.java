1404e8f7d93be2262f8554120f2801d3d17f7a14
LiubomyrPater <pater87lm@gmail.com>
2019-12-05 13:25:24 +0200

    Nested

---
+++ src/Inner/nestedClass/Test.java
@@ -0,0 +1,25 @@
+package Inner.nestedClass;
+
+public class Test {
+
+    public class Test2{}
+
+
+    public class Test3{}
+
+    private class Test4{}
+
+
+    private static class Test5{}
+
+    public static class Test6{}
+
+
+
+}
+
+class Test2{}
+
+
+class Test7{}
+
