5417ac98a7d88727044293f80ba65bc4ac59417e
LiubomyrPater <pater87lm@gmail.com>
2020-02-10 15:31:01 +0200

    js

--- src/js/two/main_script.js
+++ src/js/two/main_script.js
@@ -44,22 +44,72 @@ function one() {
 }
 
 function two() {
-    let styles = ["Jazz","Blues"]
-    styles.push("Rock 'n' roll")
-    console.log(styles)
-    styles.splice((styles.length-2),1,"Classic")
-    console.log(styles)
-    console.log(styles.splice(0,1))
-    console.log(styles)
-    styles.unshift("Rep", "Reggi")
-    console.log(styles)
+    let styles = ["Jazz","Blues"];
+    styles.push("Rock 'n' roll");
+    console.log(styles);
+    styles.splice((styles.length-2),1,"Classic");
+    console.log(styles);
+    console.log(styles.splice(0,1));
+    console.log(styles);
+    styles.unshift("Rep", "Reggi");
+    console.log(styles);
 }
 
-function three(){}
-function four(){}
-function five(){}
-function six(){}
+function three(){
+    let array = ["a","b","c","d","f"]
+    function find(arr, value) {
+        return arr.indexOf(value, 0);
+    }
+    console.log(find(array,"d"));
+}
+
+function four(){
+    let arr_int = [1,4,5,3,6,4,0,78,8,4,3,-3,-773,-463,4,56,-32,-66323,62662,-5];
+    function compareNumbers(a,b){
+        return a > b ? 1 : a < b ? -1 : 0;
+    }
+    arr_int.sort(compareNumbers);
+    console.log(arr_int);
+
+    arr_int.reverse();
+    console.log(arr_int);
+
+    let arr_positiv = arr_int.filter(function (value) { if (value >= 0) return true });
+    let arr_negative = arr_int.filter(function (value) { if (value < 0) return true });
+    console.log(arr_positiv);
+    console.log(arr_negative);
+}
+
+function five(){
+    let arr_int = [1,4,5,3,6,4,0,78,8,4,3,-3,-773,-463,4,56,-32,-66323,62662,-5];
+    function filterRange(array, a, b) {
+        return array.filter(function (value){if (value >= a && value <= b) return true});
+    }
+    let new_arr = filterRange(arr_int,-32,78);
+    console.log(new_arr);
+}
+function six(){
+    function camelize(str) {
+        return str
+            .split('-')
+            .map(
+                (text, index) => index === 0 ? text : text[0].toUpperCase() + text.slice(1)
+            )
+            .join('')
+    }
+    console.log("my-short-string");
+    console.log(camelize("my-short-string"));
+}
 function seven(){}
+
+
+
+
+
+
+
+
+
 
 console.log("-------------ONE-------------")
 one();
