28c2e217a58b0ac1fe56a7e983e6d3000cef5e64
LiubomyrPater <pater87lm@gmail.com>
2020-02-12 16:28:46 +0200

    tests

---
+++ src/cinema/test/CinemaTest.java
@@ -0,0 +1,36 @@
+package cinema.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CinemaTest {
+
+    @Test
+    public void getMoviesLibrary() {
+    }
+
+    @Test
+    public void setSchedules() {
+    }
+
+    @Test
+    public void addMovieInLibrary() {
+    }
+
+    @Test
+    public void addSeance() {
+    }
+
+    @Test
+    public void removeMovie() {
+    }
+
+    @Test
+    public void removeSeance() {
+    }
+
+    @Test
+    public void finMovie() {
+    }
+}
---
+++ src/cinema/test/CinemaTest2.java
@@ -0,0 +1,34 @@
+package cinema.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CinemaTest2 {
+
+    @org.junit.jupiter.api.Test
+    void getMoviesLibrary() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSchedules() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addMovieInLibrary() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addSeance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeMovie() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeSeance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void finMovie() {
+    }
+}
